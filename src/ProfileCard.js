e9f6863e6a000097e0f76ea7ea0d0020af5172dc
deepika joshi <upreti.deepika@gmail.com>
2023-05-11 15:34:13 +0800

    added image as props

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,10 @@
-function ProfileCard({title, handle}) {
+function ProfileCard({ title, handle, image }) {
   return (
     <div>
-  <div>Title is {title}</div>
-  <div>Handle is {handle}</div>
-  </div>
+      <img src={image} alt="pda logo"/>
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </div>
   )
 }
 export default ProfileCard;
