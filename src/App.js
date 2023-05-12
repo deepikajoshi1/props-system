5fec08bb12c7b6e3a1340b72763b203b965581f5
deepika joshi <upreti.deepika@gmail.com>
2023-05-12 11:36:04 +0800

    added css

--- src/App.js
+++ src/App.js
@@ -14,14 +14,26 @@ function App() {
         </div>
       </section>
       <div className="columns">
-        <div class="column">
-          <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+        <div class="columnis-4">
+          <ProfileCard
+          title="Alexa"
+          handle="@alexa99"
+          image={AlexaImage}
+          description="Alexa was created by Amazon and helps you buy things." />
         </div>
-        <div className="column">
-          <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+        <div className="columnis-4">
+          <ProfileCard
+          title="Cortana"
+          handle="@cortana32"
+          image={CortanaImage}
+          description="Cortana was made by Microsoft. Who knows what it does?" />
         </div>
-        <div className="column">
-          <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+        <div className="columnis-4">
+          <ProfileCard
+          title="Siri"
+          handle="@siri01"
+          image={SiriImage}
+          description="Siri was made by Apple and is being phased out"/>
         </div>
       </div>
     </div>
